8e1d98536aed12c12ff25113972c99c6b93b6ab8
Jose <arck1997@hotmail.com>
2023-10-07 21:16:19 -0600

    Funcionamiento del boton Terror, y agregar las peliculas de terro

--- buscador.js
+++ buscador.js
@@ -10,6 +10,7 @@ document.addEventListener("keyup", e=>{
 })
 
 let btnaccion= document.getElementById("btnaccion")
+let btnterror= document.getElementById("btnterror")
 let titendencia= document.getElementById("titendencia")
 let tendecia= document.getElementById("tendecia")
 let titeserie= document.getElementById("titeserie")
@@ -17,8 +18,12 @@ let series= document.getElementById("series")
 let titeaccion= document.getElementById("titeaccion")
 let accion= document.getElementById("accion")
 let nadaesSimple = document.getElementById("nadaesSimple")
+let titeterror = document.getElementById("titeterror")
+let terror = document.getElementById("terror")
 
+let carouselExampleCaptions = document.getElementById("carouselExampleCaptions")
 let peliAccionOculta= document.getElementById("peliAccionOculta")
+let peliTerrorOculta= document.getElementById("peliTerrorOculta")
 
 btnaccion.addEventListener("click", peliAccion);
 function peliAccion (){
@@ -27,7 +32,28 @@ function peliAccion (){
   tendecia.classList.add("hide");
   titeserie.classList.add("hide");
   series.classList.add("hide");
+  titeterror.classList.add("hide");
+  terror.classList.add("hide");
+  carouselExampleCaptions.classList.add("hide");
+  peliTerrorOculta.classList.add("hide")
   peliAccionOculta.classList.remove("hide")
+  accion.classList.remove("hide")
+   
+}
+
+btnterror.addEventListener("click", peliTerror);
+function peliTerror (){
+
+  titendencia.classList.add("hide");
+  tendecia.classList.add("hide");
+  titeserie.classList.add("hide");
+  series.classList.add("hide");
+  accion.classList.add("hide");
+  titeaccion.classList.add("hide");
+  carouselExampleCaptions.classList.add("hide");
+  peliAccionOculta.classList.add("hide")
+  peliTerrorOculta.classList.remove("hide")
+  terror.classList.remove("hide")
 
    
 }
